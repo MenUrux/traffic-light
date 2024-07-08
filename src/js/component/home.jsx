290d82cddee2d4a52a3fd57eb41edf119c8de700
menurux <jorgepino.utu@gmail.com>
2024-07-08 12:25:26 -0300

    traffic light

---
+++ src/js/component/home.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+//create your first component
+const Home = () => {
+	//create a state for the selected color
+	const [selectedColor, setSelectedColor] = useState("");
+
+	//function to select the traffic light color
+	function isSelected(color) {
+		let lights = document.querySelectorAll(".light");
+		lights.forEach(light => {
+			light.classList.remove("selected");
+			light.classList.remove("opacity-100");
+		});
+		if (selectedColor === color) {
+			setSelectedColor("");
+		} else {
+			setSelectedColor(color);
+		}
+	}
+
+	function randomLight() {
+		let lights = document.querySelectorAll(".light");
+		lights.forEach(light => {
+			light.classList.remove("selected");
+			light.classList.remove("opacity-100");
+			// lights.classList.add("opacity-75");
+		});
+
+		let random = Math.floor(Math.random() * lights.length);
+		lights[random].classList.add("selected");
+	}
+
+	/* 	function purpleLight() {
+			let trafficlight = document.getElementById("traffic-light");
+			console.log(trafficlight);
+	
+			if (trafficlight.childElementCount >= 4) {
+				alert("You already have this light.");
+				return;
+			}
+	
+			let purpleLightDOM = document.createElement("div");
+			purpleLightDOM.classList.add(`"light", "purple", ${selectedColor === "yellow" ? "selected" : ""}`);
+	
+			trafficlight.appendChild(purpleLightDOM);
+		}
+	 */
+
+
+
+	return (
+		<div className="container d-flex align-items-center flex-column">
+			<div id="traffic-light" className="border-2 bg-black p-4 rounded-3 gap-3 d-flex flex-column ">
+				<div
+					className={`light green ${selectedColor === "green" ? "selected" : ""}`}
+					onClick={() => isSelected("green")}
+				>
+				</div>
+				<div
+					className={`light yellow ${selectedColor === "yellow" ? "selected" : ""}`}
+					onClick={() => isSelected("yellow")}
+				>
+				</div>
+				<div
+					className={`light red ${selectedColor === "red" ? "selected" : ""}`}
+					onClick={() => isSelected("red")}
+				>
+				</div>
+			</div>
+
+			<button className="btn btn-dark my-2" onClick={randomLight}>Random</button>
+		</div>
+		// <button className="btn btn-dark my-2" onClick={purpleLight}>Add ?????? light</button> 
+	);
+};
+
+export default Home;
